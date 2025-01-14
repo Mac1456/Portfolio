d2fa0713e1942ce5ac4f3df4b1df0bc8b21ab7a8
aniqatc <aniqatc97@gmail.com>
2025-01-13 20:02:37 -0500

    fix overflow

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Header, About, Projects, Footer } from './components';
 
 export default function App() {
 	return (
-		<main className="max-w-screen-md mx-auto py-10 px-4 text-base">
+		<main className="overflow-x-hidden max-w-screen-md mx-auto py-10 px-4 text-base">
 			<motion.div
 				initial={{ opacity: 0, y: 20 }}
 				animate={{ opacity: 1, y: 0 }}
