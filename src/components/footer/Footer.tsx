d1cca334e4978ae955180d1f97a35afde40c9881
Mustafa Chaudheri <m.chaudheri@Mustafas-MacBook-Air.attlocal.net>
2025-07-28 23:14:21 -0500

    fix: Remove unused import in Footer component to fix Vercel build error

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,5 +1,3 @@
-import { personal } from '../../data';
-
 export default function Footer() {
 	return (
 		<footer className="mt-8 text-sm text-gray-5 flex justify-between gap-1 flex-wrap sm:flex-nowrap">
